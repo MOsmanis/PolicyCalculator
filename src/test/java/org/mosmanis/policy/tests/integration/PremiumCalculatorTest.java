f69f550d7e7e609c291c8e8d260f2f2345c80444
martins.osmanis <martinosmanis@gmail.com>
2022-03-23 22:22:19 +0200

    Replace integration test configuration classes with the main spring application class

--- src/test/java/org/mosmanis/policy/tests/integration/PremiumCalculatorTest.java
+++ src/test/java/org/mosmanis/policy/tests/integration/PremiumCalculatorTest.java
@@ -3,26 +3,20 @@ package org.mosmanis.policy.tests.integration;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.mosmanis.policy.AppConfig;
+import org.mosmanis.policy.PolicyApplication;
 import org.mosmanis.policy.argumentsproviders.integration.PremiumCalculatorCalculateValues;
 import org.mosmanis.policy.calculators.PremiumCalculator;
 import org.mosmanis.policy.dto.Policy;
 import org.mosmanis.policy.exceptions.PolicyCalculationException;
-import org.mosmanis.policy.factories.PremiumServiceFactory;
-import org.mosmanis.policy.services.PolicyService;
-import org.mosmanis.policy.validators.PolicyValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {AppConfig.class, PremiumServiceFactory.class, PremiumCalculator.class,
-    PolicyService.class, PolicyValidator.class})
-@WebAppConfiguration
+@ContextConfiguration(classes = PolicyApplication.class)
 public class PremiumCalculatorTest {
     @Autowired
     private ApplicationContext applicationContext;
